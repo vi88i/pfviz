92eda9127d6d8ae3cc5989664924559a94fa310d
Vighnesh Nayak S <vighneshsnayak@gmail.com>
2020-03-29 19:58:34 +0530

    fixed npm package issue

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/')));
 
 const PORT = process.env.PORT || 3000;
